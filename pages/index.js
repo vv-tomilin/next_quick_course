d87e45c70598bf39f5e977b984389df3123a598e
vv_tomilin <backatom1@gmail.com>
2021-10-27 09:08:59 +0500

    SEO + Meta

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,15 @@
+import Head from 'next/head';
 import Link from 'next/link';
 
 export default function Home() {
   return (
     <div>
+      <Head>
+        <title>Next title Home Hello</title>
+        <meta name="keywords" content="next, javascript, nextjs, react" ></meta>
+        <meta name="description" content="This is YouTube tutorial for NextJS"></meta>
+        <meta charSet="utf-8"/>
+      </Head>
       <h1>Hello NextJS</h1>
       <p>This text</p>
       <ul>
